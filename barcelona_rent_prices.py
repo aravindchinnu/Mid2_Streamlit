2db81b19f064e4c6f488d65c82588be6d25c37e5
aravindchinnu <43126640+aravindchinnu@users.noreply.github.com>
2022-12-08 13:57:04 -0500

    Update barcelona_rent_prices.py

--- barcelona_rent_prices.py
+++ barcelona_rent_prices.py
@@ -15,51 +15,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
 
-st.header("Barcelon Rent Prices")
-
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
-
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
-
-def page3():
-    st.markdown("# Page 3 ❄️")
-    st.sidebar.markdown("# Page 3 ❄️")
+st.header("Barcelona Rent Prices - 'Choose an affordable-safe neighbourhood at your preferred place'")
 
 data = pd.read_csv("data/Barcelona_rent_price.csv")
 
 df_year = data.groupby(['Year'])['Price'].mean().reset_index(name='average price')
 
-line = alt.Chart(df_year,title = "Rent Price per month in Barcelona").mark_line().encode(
-    x='Year',
-    y='average price'
+line = alt.Chart(df_year,title = "Rent over the years in Barcelona").mark_line().encode(
+    x='Year:N',
+    y=alt.Y('average price',
+    scale =alt.Scale(zero=False))
 )
 st.altair_chart(line.interactive(), use_container_width=True)
-#st.line_chart(df_year)
 
-years_list = [2014,2015,2016,2017,2018,2019,2020,2021,2022]
-trimester_list = [1,2,3]
+dataframe1  = data[data['Average _rent']== 'average rent (euro/month)']
+dataframe2  = data[data['Average _rent']== 'average rent per surface (euro/m2)']
 
-district_list = data['District'].unique()
-
-years = st.sidebar.multiselect(
-            'Choose a year',
-            years_list)
-
-districts = st.sidebar.multiselect(
-            'Choose a district',
-            district_list)
-df4 = data.groupby(['District','Neighbourhood'])['Price'].mean().reset_index(name='average price')
-if districts:
-    df5 = df4.loc[df4['District'].isin(districts)]
-    neighbourhood = st.sidebar.multiselect(
-            'Choose a Neighbourhood',
-            df5['Neighbourhood'])
-trimester = st.sidebar.multiselect(
-     'Select the trimester',
-     trimester_list)
-     
 col1,col2 = st.columns(2)
+hist = alt.Chart(dataframe1,title = "Histogram of Prices(average rent (euro/month))").mark_bar().encode(
+    alt.X("Price:Q", bin=True),
+    y='count()',
+)
+col1.altair_chart(hist, use_container_width = True)
+
+hist1 = alt.Chart(dataframe2,title = "Histogram of Prices(average rent per surface (euro/m2))").mark_bar().encode(
+    alt.X("Price:Q", bin=True),
+    y='count()',
+)
+col2.altair_chart(hist1, use_container_width = True)
+
+st.write('The average price of rental housing in Barcelona reached 17.6 euros per square metre in the first quarter of 2020, well above the Spanish average of 10.2 euros per square metre')
